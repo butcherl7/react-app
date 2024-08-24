9751df5d3d64dccb4e27a4c1880ae1a81cbb54c4
Butcher <755365600@qq.com>
2024-08-24 09:50:39 +0800

    test

--- src/views/react/effect/index.tsx
+++ src/views/react/effect/index.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import { Divider, Select, Switch } from "antd";
 import { FrownTwoTone, SmileTwoTone } from "@ant-design/icons";
-import { Switch } from "antd";
-import { useEffect, useState } from "react";
 
-export default function Effect() {
+export default function View() {
+  const [name, setName] = useState<string>("lucy");
+
+  const options = [
+    { value: "jack", label: "Jack" },
+    { value: "lucy", label: "Lucy" },
+    { value: "Yiminghe", label: "yiminghe" },
+  ];
+
+  return (
+    <div>
+      <Select value={name} options={options} onChange={(value) => setName(value)} style={{ width: 120 }} />
+      <p>My name is {name}</p>
+      <Divider />
+      <Effect />
+    </div>
+  );
+}
+
+function Effect() {
   const [emotion, setEmotion] = useState(true);
 
-  useEffect(() => {
-    console.log(123);
-  }, []);
+  console.log(123);
 
   return (
     <div>
